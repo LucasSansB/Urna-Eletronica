29e6d53a44e72b8bdc035ed17140c164fa5ed580
Lucas Santos <43860524+LucasSansB@users.noreply.github.com>
2023-10-11 15:02:20 +0000

    feat(design e logica)

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,3 +1,70 @@
+function urnaEletronica(){
+
+    let candidato1 = 0;
+    let candidato2 = 0;
+    let candidato3 = 0;
+    let branco = 0;
+    let nulo = 0;
+    let voto = 0;
+    let seletor;
+    let percentual;
+    
+    seletor = parseInt(prompt('Digite'));
+    
+    while(seletor !== 0){
+
+        switch(seletor){
+            case(1):
+                candidato1 += 1;
+                voto++;
+                console.log('votos candidato 1: ' + candidato1);
+                break
+                
+            case(2):
+                candidato2 += 1;
+                voto++;
+                console.log('votos candidato 2: ' + candidato2);
+                break
+                
+            case(3):
+                candidato3 += 1;
+                voto++;
+                console.log('votos candidato 3: ' + candidato3);
+                break
+
+            case(5):
+                branco += 1;
+                voto++;
+                console.log('votos em branco: ' + branco);
+                break
+
+            case(8):
+                nulo += 1;
+                voto++;
+                console.log('votos nulo: ' + nulo);
+                break
+        }
+        seletor = parseInt(prompt('Digite'));
+    }
+    if(candidato1 > candidato2 && candidato1 > candidato3){
+        console.log("Candidato 1 ganhou, por " + candidato1-- + " votos");
+    }else if(candidato2 > candidato1 && candidato2 > candidato3){
+        console.log("Candidato 2 ganhou, por " + candidato2-- + " votos");
+    }else if(candidato3 > candidato1 && candidato3 > candidato2){
+        console.log("Candidato 3 ganhou, por " + candidato3-- + " votos");
+    }else{
+        console.log("empatou, por " + voto-- + " votos");
+    }
+    
+    console.log('Total de votos do Candidato 1: ' + candidato1 * 100 / voto);
+    console.log('Total de votos do Candidato 1: ' + candidato1 * 100 / voto);
+    console.log('Total de votos do Candidato 2: ' + candidato2 * 100 / voto);
+    console.log('Total de votos do Candidato 3: ' + candidato3 * 100 / voto);
+    console.log('Total de votos em Branco: ' + branco * 100 / voto);
+    console.log('Total de votos Nulo: ' + nulo * 100 / voto);
+    console.log('Total de votos: ' + voto);
+}
+
 // const opcao = 1;
 // switch (opcao){
 //     case 1:
@@ -18,78 +85,8 @@
 // } else {
 //     console.log('Nenhuma das opções');
 // }
-var candidato1 = 0;
-var candidato2 = 0;
-var candidato3 = 0;
-var branco = 0;
-var nulo = 0;
-var seletor;
 
-function urnaEletronica(){
 
-    candidato1 = 0;
-    candidato2 = 0;
-    candidato3 = 0;
-    branco = 0;
-    nulo = 0;
-    
-    seletor = parseInt(prompt('Digite'));
-    
-    while(seletor !== 0){
-
-        switch(seletor){
-            case(1):
-                candidato1 += 1;
-                console.log('votos candidato 1: ' + candidato1);
-                seletor = parseInt(prompt('Digite'));
-                break
-                
-            case(2):
-                candidato2 += 1;
-                console.log('votos candidato 2: ' + candidato2);
-                seletor = parseInt(prompt('Digite'));
-                break
-                
-            case(3):
-                candidato3 += 1;
-                console.log('votos candidato 3: ' + candidato3);
-                seletor = parseInt(prompt('Digite'));
-                break
-
-            case(5):
-                branco += 1;
-                console.log('votos em branco: ' + branco);
-                seletor = parseInt(prompt('Digite'));
-                break
-
-            case(8):
-                nulo += 1;
-                console.log('votos nulo: ' + candidato3);
-                seletor = parseInt(prompt('Digite'));
-                break
-        }
-    }
-}
-
-    
-    // do{
-        
-    //     console.log('...');
-        
-    //     candidato1 = candidato1 + 1
-
-    // }while(seletor !== 0);
-
-  
-        
-function candidato(){
-    while(seletor == 0){
-        console.log('total candidato 1: ' + candidato1);
-    }
-    
-    
-
-}
 // function ue(){
 //     let opcao;
 //     let contador = 0;
