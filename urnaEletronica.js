b2bdc8bfb97f2e3e4651eea4e5ab18a62c6b80c3
lucas <lucas@senai.com>
2023-10-17 11:54:12 -0300

    feat(senha porcenatgem S/N)

--- urnaEletronica.js
+++ urnaEletronica.js
@@ -3,16 +3,37 @@ function urnaEletronica(){
     let candidato1 = 0;
     let candidato2 = 0;
     let candidato3 = 0;
+    let nomecandidato1;
+    let nomecandidato2;
+    let nomecandidato3;
+    let nome;
     let branco = 0;
     let nulo = 0;
     let voto = 0;
     let seletor;
-    let percentual;
-    
-    seletor = parseInt(prompt('Digite'));
-    
-    while(seletor !== 0){
+    let senha;
+    let acesso = 0;
+    let encerramento = "N";
 
+    
+    while(acesso == 0){
+        senha = parseInt(prompt('Digite a senha'));
+        if(senha == '123456'){
+            console.log('Acesso liberado');
+            acesso = 1;
+        }else{
+            console.log('Valor invalido');
+            acesso = 0;
+        }
+    
+    }   
+    
+   
+
+    while(encerramento != 'S'){
+
+        seletor = parseInt(prompt('Digite')); 
+        
         switch(seletor){
             case(1):
                 candidato1 += 1;
@@ -43,26 +64,43 @@ function urnaEletronica(){
                 voto++;
                 console.log('votos nulo: ' + nulo);
                 break
+
+            case(0):
+                encerramento = prompt('Se quiser encerrar a votação, digite S. Caso não, digite N');
+                
+                switch(encerramento){
+                    case('S'):
+                    encerramento = "S"
+                    break
+
+                    case('N'):
+                    encerramento = "N"
+                    break
+
+                }
+                break
         }
-        seletor = parseInt(prompt('Digite'));
+          
     }
+
     if(candidato1 > candidato2 && candidato1 > candidato3){
-        console.log("Candidato 1 ganhou, por " + candidato1-- + " votos");
+        console.log("Candidato 1 ganhou, por " + candidato1 + " votos");
     }else if(candidato2 > candidato1 && candidato2 > candidato3){
-        console.log("Candidato 2 ganhou, por " + candidato2-- + " votos");
+        console.log("Candidato 2 ganhou, por " + candidato2 + " votos");
     }else if(candidato3 > candidato1 && candidato3 > candidato2){
-        console.log("Candidato 3 ganhou, por " + candidato3-- + " votos");
+        console.log("Candidato 3 ganhou, por " + candidato3 + " votos");
     }else{
-        console.log("empatou, por " + voto-- + " votos");
+        console.log("empatou, por " + voto + " votos");
     }
     
-    console.log('Total de votos do Candidato 1: ' + candidato1 * 100 / voto);
-    console.log('Total de votos do Candidato 1: ' + candidato1 * 100 / voto);
-    console.log('Total de votos do Candidato 2: ' + candidato2 * 100 / voto);
-    console.log('Total de votos do Candidato 3: ' + candidato3 * 100 / voto);
-    console.log('Total de votos em Branco: ' + branco * 100 / voto);
-    console.log('Total de votos Nulo: ' + nulo * 100 / voto);
+    console.log('Total de votos do Candidato 1: ' + candidato1 * 100 / voto + '%');
+    console.log('Total de votos do Candidato 2: ' + candidato2 * 100 / voto + '%');
+    console.log('Total de votos do Candidato 3: ' + candidato3 * 100 / voto + '%');
+    console.log('Total de votos em Branco: ' + branco * 100 / voto + '%');
+    console.log('Total de votos Nulo: ' + nulo * 100 / voto + '%');
     console.log('Total de votos: ' + voto);
+
+
 }
 
 // const opcao = 1;
